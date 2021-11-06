600df33380dca4a41c6c19bccbe38677c96faf03
AfnanAl97 <iAfnanAlharbi9@gmail.com>
2021-11-06 14:49:03 +0300

    project

--- project-unit-2/src/App.js
+++ project-unit-2/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Productpage from './components/Productpage';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Productpage from './components/Productpage';
+import Form from './components/Form';
+
+import React from 'react';
 
 function App() {
   return (
     <>
-    <Header/>
-    <Productpage/>
-    <Footer/>
+    {/* <Header/> */}
+    <Form />
+    {/* Will be call it from header */}
+        {/* <Signup/> */}
+        {/* <Login/> */}
+    {/* <Productpage/>
+    <Footer/> */}
     </>
   );
 }
---
+++ project-unit-2/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+
+function Form() {
+    return (
+    <div>
+        <Signup/>
+        <Login/>
+    </div>
+    );
+}
+
+export default Form;
