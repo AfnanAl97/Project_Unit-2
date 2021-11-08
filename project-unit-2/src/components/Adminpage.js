6919b7d033e15393cbe27e952178dfa4afac4a1b
AfnanAl97 <iAfnanAlharbi9@gmail.com>
2021-11-08 19:23:20 +0300

    Admin page finish

--- project-unit-2/src/components/Adminpage.js
+++ project-unit-2/src/components/Adminpage.js
@@ -1,20 +1,42 @@
-import React, {useState} from "react";
+import React from "react";
+import {setProducts} from "../reducers/product/action";
+import {useDispatch} from "react-redux";
 
 function Adminpage() {
 
-    const [id, setId] = useState();
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState();
-    const [description, setDescription] = useState("");
-    const [category, setCategory] = useState("");
-    const [image, setImage] = useState();
+    const dispatch = useDispatch();
+    let arrayProduct = {};
+
+    const addProduct = () => {
+        
+        let inputId = prompt("Product id: ")
+        let inputName = prompt("Product name: ")
+        // console.log(inputVal)
+        let inputPrice = prompt("Product price: ")
+        let inputDescription = prompt("Product description: ")
+        let inputCategory = prompt("Product category: ")
+        let inputImage = prompt("Product image: ")
+        arrayProduct = {
+            id: inputId, 
+            name: inputName, 
+            price: inputPrice, 
+            description: inputDescription,
+            category: inputCategory,
+            image: inputImage
+        }
+        const action = setProducts(arrayProduct) 
+        dispatch(action)
+        
+    }
 
     return (
         <>
         <div className="admin-page">
          <div className="admin-card">
            <div className="admin-btnn">
-              <p className="admin-btn"><a href="#">+</a></p>
+            <form>
+              <p className="admin-btn" onClick={addProduct}><a href="#">+</a></p>
+            </form>
            </div>
          </div>
         </div>
